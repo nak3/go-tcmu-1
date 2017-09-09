330d5f9caa215399114987f0a5bb6c2197733887
Kenjiro Nakayama <nakayamakenjiro@gmail.com>
2017-09-10 00:06:28 +0900

    clean

--- netlink.go
+++ netlink.go
@@ -1,5 +1,3 @@
-// tcmu is a package that connects to the TCM in Userspace kernel module, a part of the LIO stack. It provides the
-// ability to emulate a SCSI storage device in pure Go.
 package tcmu
 
 import (
@@ -40,12 +38,6 @@ const (
 	TCMU_CMD_SET_FEATURES
 )
 
-func enabled() []byte {
-	o := make([]byte, 1)
-	nlenc.PutUint8(o, 1)
-	return o
-}
-
 func handleNetlink() error {
 	c, err := genetlink.Dial(nil)
 	if err != nil {
@@ -64,8 +56,12 @@ func handleNetlink() error {
 		}
 	}
 	if groupID == 0 {
-		//TODO This must be not necessary as GetFamily already worked.
 		return fmt.Errorf("not found groupdID")
+	}
+
+	if family.Version < 2 {
+		logrus.Info("netlink communication is disabled, as kernel does not support it")
+		return nil
 	}
 
 	if err := c.JoinGroup(groupID); err != nil {
@@ -87,28 +83,31 @@ func handleNetlink() error {
 	}
 	_, err = c.Send(req, family.ID, netlink.HeaderFlagsRequest)
 	if err != nil {
-		logrus.Fatalf("failed to execute: %v", err)
+		logrus.Fatalf("failed to enabled to netlink: %v", err)
+		return err
 	}
 
 	for {
-		logrus.Debugf("receiving: ...")
 		msgs, _, err := c.Receive()
 		if err != nil {
-			fmt.Printf("failed to receive: %v\n", err)
+			logrus.Errorf("failed to receive netlink: %v\n", err)
 			continue
 		}
-		fmt.Printf(" %#v \n", msgs)
-		atbs, _ := netlink.UnmarshalAttributes(msgs[0].Data)
-		fmt.Printf(" %#v \n", atbs)
+		if len(msgs) != 1 {
+			//TODO
+		}
+
+		atbs, err := netlink.UnmarshalAttributes(msgs[0].Data)
+		if err != nil {
+			//TODO
+		}
 		deviceID := make([]byte, 4)
 
 		for i, _ := range atbs {
 			if atbs[i].Type == 0x8 {
 				deviceID = atbs[i].Data
 			}
-			//fmt.Printf("---data -- > %s \n", atbs[i].Data)
 		}
-		fmt.Printf("---data -- > %#v \n", atbs[1].Data)
 
 		switch msgs[0].Header.Command {
 		case TCMU_CMD_ADDED_DEVICE:
@@ -134,8 +133,6 @@ func handleNetlink() error {
 func handleNetlinkReply(c *genetlink.Conn, family *genetlink.Family, s uint32, deviceID []byte, done_cmd uint8) error {
 	status := make([]byte, 4)
 	nlenc.PutUint32(status, s)
-	one := make([]byte, 1)
-	nlenc.PutUint8(one, 0)
 
 	attrs := []netlink.Attribute{
 		{
@@ -151,7 +148,10 @@ func handleNetlinkReply(c *genetlink.Conn, family *genetlink.Family, s uint32, d
 			Data: deviceID,
 		},
 	}
-	data, _ := netlink.MarshalAttributes(attrs)
+	data, err := netlink.MarshalAttributes(attrs)
+	if err != nil {
+		logrus.Errorf("failed to marshal attributes: %v", err)
+	}
 
 	req := genetlink.Message{
 		Header: genetlink.Header{
@@ -160,9 +160,15 @@ func handleNetlinkReply(c *genetlink.Conn, family *genetlink.Family, s uint32, d
 		},
 		Data: data,
 	}
-	_, err := c.Send(req, family.ID, netlink.HeaderFlagsRequest)
+	_, err = c.Send(req, family.ID, netlink.HeaderFlagsRequest)
 	if err != nil {
-		logrus.Fatalf("failed to execute: %v", err)
+		logrus.Fatalf("failed to send: %v", err)
 	}
 	return err
+}
+
+func enabled() []byte {
+	o := make([]byte, 1)
+	nlenc.PutUint8(o, 1)
+	return o
 }
